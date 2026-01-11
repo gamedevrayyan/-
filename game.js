5236c1976053534aa8de1e37820ed64bae5e504c
gamedevrayyan <rayyan.xylem.study@gmail.com>
2026-01-11 11:52:01 +0530

    Update game.js

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d");
 
 // ================= IMAGES =================
 const playerImg = new Image();
-playerImg.src = "assets/player3.png";
+playerImg.src = "assets/player1.png";
 
 const obstacleImg = new Image();
 obstacleImg.src = "assets/obstacle.jpg";
@@ -154,6 +154,7 @@ function update() {
 }
 
 update();
+
 
 
 
